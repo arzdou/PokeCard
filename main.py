31192f8c09ce04bf787acc00c553f9e56c81ee9b
jaiquark <jaimetravesedo@hotmail.com>
2020-12-16 14:46:47 +0100

    first test for the API

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import tweepy
-
+import json
 
 def main():
-    pass
+    with open("keys.json", "r") as infile:
+        api_key, api_skey, access_token, access_stoken = json.load(infile).values()
+
+    auth = tweepy.OAuthHandler(api_key, api_skey)
+    auth.set_access_token(access_token, access_stoken)
+
+    api = tweepy.API(auth)
+
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print(tweet.text)
 
 if __name__=="__main__":
     main()
