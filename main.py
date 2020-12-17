56f116e06954ca5e82822d7455cef82f2e682a2a
jaiquark <jaimetravesedo@hotmail.com>
2020-12-17 10:47:34 +0100

    no replies to threads

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from card_scripts import create_card
 from time import sleep
 import tweepy
@@ -13,16 +14,15 @@ class TweetsListener(tweepy.StreamListener):
         print("Estoy conectado")
 
     def on_status(self, status):
-        print("Creando trajeta para "+status.user.name)
+        if status.in_reply_to_status_id is not None: return
         create_card(status)
-        print("Carta creada")
         card_media = self.api.media_upload("output.png")
         sleep(10)
         self.api.update_status("Aquí tienes @{}!".format(status.user.screen_name),
                                status.id,
                                media_ids = [card_media.media_id],
                                auto_populate_reply_metadata=True)
-        print("Se ha enviado el tweet")
+        print("Tarjeta creada para "+status.user.name)
 
     def on_error(self, status_code):
         print("Oh no, ha ocurrido un error", status_code)
@@ -38,7 +38,7 @@ def main():
     api = tweepy.API(auth, wait_on_rate_limit=True,
                      wait_on_rate_limit_notify=True)
 
-    api.update_status("Jaime que feo eres @Arzdou")
+
     stream = TweetsListener(api)
     sreamingApi = tweepy.Stream(auth=api.auth, listener=stream)
 
